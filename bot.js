eedf1c1b171c98d123bde788f05260853e48f6c4
sokr1 <67761831+sokr1@users.noreply.github.com>
2020-07-09 07:09:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!help !bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`!help !bc |DF_Team`,"")
 client.user.setStatus("dnd")
 });
 
